59034f23b850681a9a799f3e52405f73e3f9d1a9
Sophiessa Phillipp <sophiessaphillipp@gmail.com>
2022-06-12 18:21:39 +0600

    minor changes

--- osc/osc-server.js
+++ osc/osc-server.js
@@ -1,10 +1,11 @@
 const osc = require('node-osc');
 
-const serverPort = 3333;
+const serverPort = 1234;
 const serverIP = '192.168.0.25';
 const oscServer = new osc.Server(serverPort, serverIP);
 
 
 oscServer.on('message', (msg, rinfo) => {
-    console.log('Received data: ', msg, rinfo);
+    console.log('Message coming from ' + rinfo.address + ':' + rinfo.port);
+    console.log('Message: ' + msg[1]);
 });
