f7d5b612f277df2187e9cecfbea1def2ea6c27b8
Sophiessa Philipp <90568185+sophiessa@users.noreply.github.com>
2022-06-24 15:50:53 +0600

    added support of osc messages without communicating to neo6m

---
+++ osc/neo6m-esp8266-osc/neo6m-esp8266-osc.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+
+char ssid[] = "WIFI_SSID";
+char pass[] = "WIFI_PASSWORD";
+
+WiFiUDP Udp;
+
+const IPAddress outIp(192,168,0,25); //IP address of the OSC server
+const unsigned int outPort = 1234; //Port of the OSC server
+const unsigned int localPort = 1235; //Local port
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting to ");Serial.println(ssid);
+  
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(".");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP Communication");
+  Udp.begin(localPort);
+}
+
+void loop() {
+  OSCMessage msg("/message");
+  msg.add("test");
+  Udp.beginPacket(outIp, outPort);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+}
